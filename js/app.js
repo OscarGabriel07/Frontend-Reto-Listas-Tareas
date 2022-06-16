cf473b5fc5e58975192e08a5d8dd524786e9c6f4
Oscar Farfan <oscarfarfan92@gmail.com>
2022-06-16 14:54:37 -0500

    Se crean funciones para editar y eliminar tareas

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ const getAll = async () => {
             const containerListOfTasksHeader = document.createElement('div');
             containerListOfTasksHeader.className = "list-of-tasks-header";
             const labelList = document.createElement('label');
-            labelList.textContent = element.name;
+            labelList.textContent = element.name.toUpperCase();
             const inputDeleteList = document.createElement('input');
             inputDeleteList.id = element.id;
             inputDeleteList.type = "button";
@@ -30,6 +30,7 @@ const getAll = async () => {
             botonCreateTask.className = "btn-create-task";
             botonCreateTask.value = "Crear Tarea";
             botonCreateTask.dataset.id = element.id;
+            botonCreateTask.id = element.id + "-btn-update-task";
             inputCreateTask.id = element.id + "-" + botonCreateTask.className;
             containerListOfTasksHeader.appendChild(labelList);
             containerListOfTasksHeader.appendChild(inputDeleteList);
@@ -77,10 +78,16 @@ const getAll = async () => {
                 buttonEdit.type = "button";
                 buttonEdit.value = "Editar";
                 buttonEdit.className = "btn-edit-task";
+                buttonEdit.dataset.idList = element.id;
+                buttonEdit.dataset.description = element.tasks[i].description;
+                buttonEdit.dataset.id = element.tasks[i].id;
+                buttonEdit.dataset.completed = element.tasks[i].completed;
                 const buttonDelete = document.createElement('input');
                 buttonDelete.type = "button";
                 buttonDelete.value = "Eliminar";
                 buttonDelete.className = "btn-delete-task";
+                buttonDelete.dataset.idList = element.id;
+                buttonDelete.dataset.idTask = element.tasks[i].id;
                 tdAcciones.appendChild(buttonEdit);
                 tdAcciones.appendChild(buttonDelete);
                 trBody.appendChild(tdId);
@@ -146,7 +153,7 @@ const createNewList = () => {
 };
 
 /**
- * Esta función se crea para la delegación de eventos del DOM
+ * Esta función se crea para la delegación del evento click del DOM
  */
 const eventsButtons = () => {
 
@@ -156,7 +163,7 @@ const eventsButtons = () => {
          * Función para el evento de eliminar una lista
          */
         if (e.target.className === "btn-delete-list") {
-            let isDelete = confirm(`¿Estás seguro de eliminar la lista?`);
+            let isDelete = confirm("¿Estás seguro de eliminar la lista?");
 
             if (isDelete) {
                 try {
@@ -175,37 +182,105 @@ const eventsButtons = () => {
         }
 
         /**
-         * Función para el evento de crear una nueva tarea
+         * Función para el evento de crear una nueva tarea y actualizar una tarea
          */
         if (e.target.className === "btn-create-task") {
             let idInputTask = e.target.dataset.id + "-" + e.target.className;
             const inputCreateTask = document.getElementById(idInputTask);
-            if (inputCreateTask.value !== "") {
-                try {
-                    let options = {
-                        method: "POST",
-                        headers: {
-                            "Content-type": "application/json; charset=utf-8"
-                        },
-                        body: JSON.stringify({
-                            lista: {
-                                id: e.target.dataset.id
+
+            if (e.target.value === "Crear Tarea") {
+                if (inputCreateTask.value !== "") {
+                    try {
+                        let options = {
+                            method: "POST",
+                            headers: {
+                                "Content-type": "application/json; charset=utf-8"
                             },
-                            description: inputCreateTask.value
-                        })
+                            body: JSON.stringify({
+                                lista: {
+                                    id: e.target.dataset.id
+                                },
+                                description: inputCreateTask.value
+                            })
+                        }
+                        let res = await fetch("http://localhost:8080/task", options);
+                        let json = await res.json();
+
+                        location.reload();
+                    } catch (error) {
+
                     }
-                    let res = await fetch("http://localhost:8080/task", options);
-                    let json = await res.json();
-
-                    location.reload();
-                } catch (error) {
-
+                } else {
+                    alert("¡Por favor ingrese la descripción de la tarea que desea crear!");
                 }
+
             } else {
-                alert("¡Por favor ingrese la descripción de la tarea que desea crear!");
+                if (inputCreateTask.value !== "") {
+                    console.log("Entré al if de editar la tarea");
+                    console.log(inputCreateTask.value);
+                    console.log(inputCreateTask.dataset.idTask);
+                    console.log(inputCreateTask.dataset.completed);
+                    console.log(e.target.dataset.id);
+
+                    try {
+                        let options = {
+                            method: "PUT",
+                            headers: {
+                                "Content-type": "application/json; charset=utf-8"
+                            },
+                            body: JSON.stringify({
+                                description: inputCreateTask.value,
+                                completed: inputCreateTask.dataset.completed,
+                                lista: {
+                                    id: e.target.dataset.id
+                                }
+                            })
+                        }
+                        let res = await fetch(`http://localhost:8080/task/${inputCreateTask.dataset.idTask}`, options);
+                        let json = await res.json();
+                        location.reload();
+                    } catch (error) {
+
+                    }
+                } else {
+                    alert("¡Por favor ingrese la descripción de la tarea que está editando!");
+                }
             }
         }
 
+        /**
+         * Función para editar una tarea
+         */
+        if (e.target.className === "btn-edit-task") {
+            let idInputTaskEdit = e.target.dataset.idList + "-" + "btn-create-task";
+            const inputTaskEdit = document.getElementById(idInputTaskEdit);
+            inputTaskEdit.value = e.target.dataset.description;
+            inputTaskEdit.dataset.idTask = e.target.dataset.id;
+            inputTaskEdit.dataset.completed = e.target.dataset.completed;
+
+            let idButtonUpdateTask = e.target.dataset.idList + "-btn-update-task";
+            const buttonUpdateTask = document.getElementById(idButtonUpdateTask);
+            buttonUpdateTask.value = "Actualizar";
+        }
+
+        if (e.target.className === "btn-delete-task") {
+            let isDeleteTask = confirm("¿Estás seguro de eliminar la tarea?");
+
+            if (isDeleteTask) {
+                try {
+                    let options = {
+                        method: "DELETE",
+                        headers: {
+                            "Content-type": "application/json; charset=utf-8"
+                        }
+                    }
+                    let res = await fetch(`http://localhost:8080/task/${e.target.dataset.idTask}`, options);
+                    location.reload();
+                    let json = await res.json();
+
+                } catch (error) {}
+            }
+        }
 
     })
 }
